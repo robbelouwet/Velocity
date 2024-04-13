14ad85a5506c897554e5076f8e79bd60eec0f3da
Robbe Louwet <robbe.louwet@nara.to>
2024-04-13 16:05:59 +0200

    forgot to copy the template files into /data

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,5 +5,7 @@ echo "JVM_ARGS: ${JVM_ARGS}"
 # Inject velocity secret
 echo "$VELOCITY_SECRET" > /forwarding.secret
 
+cp -R /template-files /data
+
 # Run the server
 java $JVM_ARGS -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar /velocity*.jar
